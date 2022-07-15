757a419c91f6483710312c765c0564a002af0295
Fisher <yverrr@gmail.com>
2022-07-15 11:56:51 +0800

    小试牛刀，上传2题

---
+++ C++语言/C++语言入门/1000_入门测试题目.cpp
@@ -0,0 +1,15 @@
+/**
+ * 求两个整数的和。
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main()
+ {
+     int a, b;
+     cin >> a >> b;
+     cout << a + b << endl;
+     return 0;
+ }
---
+++ C++语言/C++语言入门/1001_helloworld.cpp
@@ -0,0 +1,13 @@
+/**
+ * 编写一个能够输出“Hello,World!”的程序。
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main()
+ {
+     cout << "Hello,World!";
+     return 0;
+ }
